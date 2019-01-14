be95259ca7762389819b429c215a72cdc8ef6765
Bud Marrical <amarrical@gmail.com>
2019-01-14 09:47:18 -0600

    Step 1 before hero class

---
+++ src/AngularBlazor.UI.Shared/Hero.cs
@@ -0,0 +1,13 @@
+﻿namespace AngularBlazor.UI.Shared
+{
+    public class Hero
+    {
+        #region [ Properties ]
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        #endregion
+    }
+}
